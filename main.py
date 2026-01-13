22af251e0241786cd913e29ceadd271073ec4068
Zachi259 <zachosterblad@gmail.com>
2026-01-13 18:42:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ while True:
         # =========================
         # SKICKA PRE-MARKET RAPPORT (14:30–14:31)
         # =========================
-        if now.hour == 20 and 38 <= now.minute < 41:
+        if now.minute % 2 == 0:
             if last_report_date != now.date():
 
                 if news_counter:
